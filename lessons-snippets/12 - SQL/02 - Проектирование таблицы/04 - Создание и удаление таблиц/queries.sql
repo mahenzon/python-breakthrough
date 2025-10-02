dd5b7853669eb1606b734d1d69178dbabca3a36a
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-10-02 13:26:36 +0300

    Update 12 02 04

--- lessons-snippets/12 - SQL/02 - Проектирование таблицы/04 - Создание и удаление таблиц/queries.sql
+++ lessons-snippets/12 - SQL/02 - Проектирование таблицы/04 - Создание и удаление таблиц/queries.sql
@@ -1,13 +1,15 @@
 DROP TABLE demo
 
-SELECT * FROM fruits;
+SELECT *
+FROM fruits;
+
+CREATE TABLE fruits
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
 
 INSERT INTO fruits ("name")
 VALUES ('Cherry');
 
 DROP TABLE fruits;
-
-CREATE TABLE fruits(
-   id BIGSERIAL PRIMARY KEY,
-   name VARCHAR NOT NULL
-);
