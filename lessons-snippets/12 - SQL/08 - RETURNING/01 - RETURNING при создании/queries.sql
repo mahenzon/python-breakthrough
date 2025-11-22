8d352c0479bed73c944c96cb9bb3dd33aacbe5f7
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-11-22 15:45:46 +0300

    Update 12 08 01

--- lessons-snippets/12 - SQL/08 - RETURNING/01 - RETURNING при создании/queries.sql
+++ lessons-snippets/12 - SQL/08 - RETURNING/01 - RETURNING при создании/queries.sql
@@ -19,7 +19,7 @@ returning id, created_at, status;
 
 insert into products (name)
 values ('Foo'),
-	   ('Fizz'),
+       ('Fizz'),
        ('Bar'),
        ('Buzz')
 returning *;
@@ -27,7 +27,7 @@ returning *;
 
 insert into products (name, status, price)
 values ('Foo 1', default, 42),
-	   ('Fizz 1', 'in_stock', 123),
+       ('Fizz 1', 'in_stock', 123),
        ('Bar 1', 'out_of_stock', default),
        ('Buzz 1', 'discontinued', 555)
 returning *;
